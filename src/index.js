e8721e4b9bb165a12a02a17b84247b5d9a3e0d3a
wilton R Silva <wilton.r.silva@grupoboticario.com.br>
2023-09-15 11:46:02 -0300

    Wilton -> Vinicius

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ const papel = 1;
 const tesoura = 2;
 const pedra = 3;
 
-function Jogar(valor1, valor2){
-    return valor1; 
+const resultado = {};
+
+function Jogar(valor1, valor2) {
+  if (valor1 == valor2) {
+    return valor1;
+  }
+
+  if (
+    (valor1 == pedra && valor2 == tesoura) ||
+    (valor1 == tesoura && valor2 == papel) ||
+    (valor1 == papel && valor2 == pedra)
+  ) {
+    return valor1;
+  }
+
+  return valor2;
 }
 
-module.exports ={
-    papel,
-    tesoura,
-    pedra,
-    Jogar
-}
+module.exports = {
+  papel,
+  tesoura,
+  pedra,
+  Jogar,
+};
--- src/unit.test.js
+++ src/unit.test.js
@@ -1,40 +1,40 @@
-const { Jogar, pedra, papel, tesoura } = require('./index.js')
+const { Jogar, pedra, papel, tesoura } = require("./index.js");
 
 //papel
-test('papel x tesoura', () => {
-    expect(Jogar(papel, tesoura)).toBe(tesoura);
-  });
+test("papel x tesoura", () => {
+  expect(Jogar(papel, tesoura)).toBe(tesoura);
+});
 
-  test('papel x tesoura', () => {
-    expect(Jogar(papel, tesoura)).toBe(papel);
-  });
+test("papel x tesoura", () => {
+  expect(Jogar(papel, tesoura)).toBe(papel);
+});
 
-  test('papel x papel', () => {
-    expect(Jogar(2)).toBe(3);
-  });
+test("papel x papel", () => {
+  expect(Jogar(papel, papel)).toBe(papel);
+});
 
-  //pedra
-  test('pedra x tesoura', () => {
-    expect(sum1(2)).toBe(3);
-  });
+//pedra
+test("pedra x tesoura", () => {
+  expect(sum1(2)).toBe(3);
+});
 
-  test('pedra x tesoura', () => {
-    expect(sum1(2)).toBe(3);
-  });
+test("pedra x tesoura", () => {
+  expect(sum1(2)).toBe(3);
+});
 
-  test('pedra x pedra', () => {
-    expect(sum1(2)).toBe(3);
-  });
+test("pedra x pedra", () => {
+  expect(sum1(2)).toBe(3);
+});
 
-  //tesoura
-  test('tesoura x papel', () => {
-    expect(sum1(2)).toBe(3);
-  });
+//tesoura
+test("tesoura x papel", () => {
+  expect(sum1(2)).toBe(3);
+});
 
-  test('tesoura x pedra', () => {
-    expect(sum1(2)).toBe(3);
-  });
+test("tesoura x pedra", () => {
+  expect(sum1(2)).toBe(3);
+});
 
-  test('tesoura x tesoura', () => {
-    expect(sum1(2)).toBe(3);
-  });
+test("tesoura x tesoura", () => {
+  expect(sum1(2)).toBe(3);
+});
